3529fdbb335a442b39f4045d463891289afdfeae
George Apostolopoulos <gapost@ipta.demokritos.gr>
2015-01-09 17:20:55 +0200

    Work on daqmx_base.cpp

--- dev/daqmx_base.cpp
+++ dev/daqmx_base.cpp
@@ -7,7 +7,7 @@
 // functor encapsulating a DAQmx Base function
 struct mxFunc
 {
-    TaskHandle& h;
+    TaskHandle h;
     int32 retval;
     QString& errmsg;
     virtual void operator()() = 0;
@@ -16,7 +16,7 @@ struct mxFunc
         if (retval!=0) errmsg = daqmx::getErrorMessage(0);
     }
 
-    mxFunc(TaskHandle& ah, QString& aerrmsg) : h(ah), errmsg(aerrmsg)
+    mxFunc(TaskHandle ah, QString& aerrmsg) : h(ah), errmsg(aerrmsg)
     {}
 };
 
@@ -126,7 +126,7 @@ QString daqmx::getErrorMessage(int c)
     }
     else return QString();
 }
-
+/////////////////////////////////////////////////////////
 struct mxCreateTask : public mxFunc
 {
     QString name;
@@ -136,108 +136,300 @@ struct mxCreateTask : public mxFunc
         checkError();
     }
 
-    mxCreateTask(const QString& aname, TaskHandle& h, QString& msg) : mxFunc(h,msg), name(aname)
+    mxCreateTask(const QString& aname, QString& msg) : mxFunc(0,msg), name(aname)
     {}
 };
 int daqmx::createTask(const QString &name, TaskHandle &h, QString &errmsg)
 {
-    mxCreateTask mxf(name, h, errmsg);
+    mxCreateTask mxf(name, errmsg);
+    daqmx::thread_->call_daqmx(&mxf);
+    h = mxf.h;
+    return mxf.retval;
+}
+/////////////////////////////////////////////////////////
+struct mxClearTask : public mxFunc
+{
+    virtual void operator()()
+    {
+        retval = DAQmxBaseClearTask(h);
+        checkError();
+    }
+
+    mxClearTask(TaskHandle h, QString& msg) : mxFunc(h,msg)
+    {}
+};
+int daqmx::clearTask(TaskHandle h, QString &errmsg)
+{
+    mxClearTask mxf(h, errmsg);
     daqmx::thread_->call_daqmx(&mxf);
     return mxf.retval;
 }
-int daqmx::clearTask(TaskHandle h, QString &errmsg)
+/////////////////////////////////////////////////////////
+struct mxStartTask : public mxFunc
 {
-    int ret = DAQmxClearTask(h);
-    if (ret!=0) errmsg = getErrorMessage(ret);
-    return ret;
-}
+    virtual void operator()()
+    {
+        retval = DAQmxBaseStartTask(h);
+        checkError();
+    }
+
+    DAQmxStartTask(TaskHandle h, QString& msg) : mxFunc(h,msg)
+    {}
+};
 int daqmx::startTask(TaskHandle h, QString &errmsg)
 {
-    int ret = DAQmxStartTask(h);
-    if (ret!=0) errmsg = getErrorMessage(ret);
-    return ret;
+    mxStartTask mxf(h, errmsg);
+    daqmx::thread_->call_daqmx(&mxf);
+    return mxf.retval;
 }
+/////////////////////////////////////////////////////////
+struct mxStopTask : public mxFunc
+{
+    virtual void operator()()
+    {
+        retval = DAQmxStopTask(h);
+        checkError();
+    }
+
+    mxStopTask(TaskHandle h, QString& msg) : mxFunc(h,msg)
+    {}
+};
 int daqmx::stopTask(TaskHandle h, QString &errmsg)
 {
-    int ret = DAQmxStopTask(h);
-    if (ret!=0) errmsg = getErrorMessage(ret);
-    return ret;
+    mxStopTask mxf(h, errmsg);
+    daqmx::thread_->call_daqmx(&mxf);
+    return mxf.retval;
 }
+/////////////////////////////////////////////////////////
+struct mxCreateAIChan : public mxFunc
+{
+    QString name;
+    int32 config;
+    double minVal, maxVal;
+    virtual void operator()()
+    {
+        retval = DAQmxBaseCreateAIVoltageChan(h,
+                                          name.toLatin1().constData(),
+                                          NULL,
+                                          config, minVal, maxVal, DAQmx_Val_Volts, NULL);
+        checkError();
+    }
+
+    mxCreateAIChan(TaskHandle h, const QString &aname,
+                   int32 aconfig, double aminVal, double amaxVal, QString& msg) : mxFunc(h,msg),
+        name(aname), config(aconfig), minVal(aminVal), maxVal(amaxVal)
+    {}
+};
 int daqmx::createAIChan(TaskHandle h, const QString &name, int32 config, double minVal, double maxVal, QString &errmsg)
 {
-    int ret = DAQmxCreateAIVoltageChan(h,
-                                   name.toLatin1().constData(),
-                                   NULL,
-                                   config, minVal, maxVal, DAQmx_Val_Volts, NULL);
-    if (ret!=0) errmsg = getErrorMessage(ret);
-    return ret;
+    mxCreateAIChan mxf(h,name,config,minVal,maxVal,errmsg);
+    daqmx::thread_->call_daqmx(&mxf);
+    return mxf.retval;
 }
+/////////////////////////////////////////////////////////
+struct mxCreateAOChan : public mxFunc
+{
+    QString name;
+    double minVal, maxVal;
+    virtual void operator()()
+    {
+        retval = DAQmxBaseCreateAOVoltageChan(h,
+                                          name.toLatin1().constData(),
+                                          NULL,
+                                          minVal, maxVal, DAQmx_Val_Volts, NULL);
+        checkError();
+    }
+
+    mxCreateAOChan(TaskHandle h, const QString &aname,
+                   double aminVal, double amaxVal, QString& msg) : mxFunc(h,msg),
+        name(aname), config(aconfig), minVal(aminVal), maxVal(amaxVal)
+    {}
+};
 int daqmx::createAOChan(TaskHandle h, const QString &name, double minVal, double maxVal, QString &errmsg)
 {
-    int ret = DAQmxCreateAOVoltageChan(h,
-                                   name.toLatin1().constData(),
-                                   NULL,
-                                   minVal, maxVal, DAQmx_Val_Volts, NULL);
-    if (ret!=0) errmsg = getErrorMessage(ret);
-    return ret;
+    mxCreateAOChan mxf(h,name,minVal,maxVal,errmsg);
+    daqmx::thread_->call_daqmx(&mxf);
+    return mxf.retval;
 }
+/////////////////////////////////////////////////////////
+struct mxCreateDIChan : public mxFunc
+{
+    QString name;
+    virtual void operator()()
+    {
+        retval = DAQmxBaseCreateDIChan(h,name.toLatin1().constData(),
+                                   NULL, DAQmx_Val_ChanForAllLines);
+        checkError();
+    }
+
+    mxCreateDIChan(TaskHandle h, const QString &aname, QString& msg) : mxFunc(h,msg), name(aname)
+    {}
+};
 int daqmx::createDIChan(TaskHandle h, const QString& name, QString& errmsg)
 {
-    int ret = DAQmxCreateDIChan(h,name.toLatin1().constData(),NULL, DAQmx_Val_ChanForAllLines);
-    if (ret!=0) errmsg = getErrorMessage(ret);
-    return ret;
+    mxCreateDIChan mxf(h,name,errmsg);
+    daqmx::thread_->call_daqmx(&mxf);
+    return mxf.retval;
 }
+/////////////////////////////////////////////////////////
+struct mxCreateDOChan : public mxFunc
+{
+    QString name;
+    virtual void operator()()
+    {
+        retval = DAQmxBaseCreateDOChan(h,name.toLatin1().constData(),
+                                   NULL, DAQmx_Val_ChanForAllLines);
+        checkError();
+    }
+
+    mxCreateDOChan(TaskHandle h, const QString &aname, QString& msg) : mxFunc(h,msg), name(aname)
+    {}
+};
 int daqmx::createDOChan(TaskHandle h, const QString& name, QString& errmsg)
 {
-    int ret = DAQmxCreateDOChan(h,name.toLatin1().constData(),NULL, DAQmx_Val_ChanForAllLines);
-    if (ret!=0) errmsg = getErrorMessage(ret);
-    return ret;
+    mxCreateDOChan mxf(h,name,errmsg);
+    daqmx::thread_->call_daqmx(&mxf);
+    return mxf.retval;
 }
+/////////////////////////////////////////////////////////
+struct mxCreateCICountEdgesChan : public mxFunc
+{
+    QString name;
+    int32 edge, countDir;
+    virtual void operator()()
+    {
+        retval = DAQmxBaseCreateCICountEdgesChan(h, name.toLatin1().constData(),
+                                             NULL, edge, 0, countDir);
+        checkError();
+    }
+
+    mxCreateCICountEdgesChan(TaskHandle h, const QString &aname,
+                             int32 aedge, int32 acountDir, QString& msg) : mxFunc(h,msg),
+        name(aname), edge(aedge), countDir(acountDir)
+    {}
+};
 int daqmx::createCICountEdgesChan(TaskHandle h, const QString& name, int32 edge, int32 countDir, QString& errmsg)
 {
-    int ret = DAQmxCreateCICountEdgesChan(h, name.toLatin1().constData(),
-                                      NULL, edge, 0, countDir);
-    if (ret!=0) errmsg = getErrorMessage(ret);
-    return ret;
+    mxCreateCICountEdgesChan mxf(h,name,edge,countDir,errmsg);
+    daqmx::thread_->call_daqmx(&mxf);
+    return mxf.retval;
 }
+/////////////////////////////////////////////////////////
+struct mxReadAnalog : public mxFunc
+{
+    const double& timo;
+    double* buff;
+    int32& read;
+    virtual void operator()()
+    {
+        uInt32 ns = read;
+        retval = DAQmxBaseReadAnalogF64(h,1,timo,DAQmx_Val_GroupByChannel,
+                                    buff,ns,&read,NULL);
+        checkError();
+    }
+
+    mxReadAnalog(TaskHandle h, const double &atimo, double* abuff, int32& aread, QString& msg) : mxFunc(h,msg),
+        timo(atimo), buff(abuff), read(aread)
+    {}
+};
 int daqmx::readAnalog(TaskHandle h, double timo, double *buff, int32 &read, QString &msg)
 {
-    bool32 _r;
-    int ret = DAQmxReadAnalogF64(h,1,timo,DAQmx_Val_GroupByChannel,
-                                 buff,1,&read,&_r);
-    if (ret!=0) msg = getErrorMessage(ret);
-    return ret;
+    mxReadAnalog mxf(h,timo,buff,read,errmsg);
+    daqmx::thread_->call_daqmx(&mxf);
+    return mxf.retval;
 }
+/////////////////////////////////////////////////////////
+struct mxReadDigital : public mxFunc
+{
+    const double& timo;
+    uInt32* buff;
+    int32& read;
+    virtual void operator()()
+    {
+        uInt32 ns = read;
+        retval = DAQmxBaseReadDigitalU32(h,1,timo,DAQmx_Val_GroupByChannel,
+                                    buff,ns,&read,NULL);
+        checkError();
+    }
+
+    mxReadDigital(TaskHandle h, const double &atimo, double* abuff, int32& aread, QString& msg) : mxFunc(h,msg),
+        timo(atimo), buff(abuff), read(aread)
+    {}
+};
 int daqmx::readDigital(TaskHandle h, double timo, uInt32* buff, int32 &read, QString &msg)
 {
-    bool32 _r;
-    int ret = DAQmxReadDigitalU32(h,1,timo,DAQmx_Val_GroupByChannel,
-                                 buff,1,&read,&_r);
-    if (ret!=0) msg = getErrorMessage(ret);
-    return ret;
+    mxReadDigital mxf(h,timo,buff,read,errmsg);
+    daqmx::thread_->call_daqmx(&mxf);
+    return mxf.retval;
 }
+/////////////////////////////////////////////////////////
+struct mxReadCounter : public mxFunc
+{
+    const double& timo;
+    uInt32* buff;
+    int32& read;
+    virtual void operator()()
+    {
+        uInt32 ns = read;
+        retval = DAQmxBaseReadCounterU32(h,1,timo,DAQmx_Val_GroupByChannel,
+                                    buff,ns,&read,NULL);
+        checkError();
+    }
+
+    mxReadCounter(TaskHandle h, const double &atimo, double* abuff, int32& aread, QString& msg) : mxFunc(h,msg),
+        timo(atimo), buff(abuff), read(aread)
+    {}
+};
 int daqmx::readCounter(TaskHandle h, double timo, uInt32* buff, int32 &read, QString &msg)
 {
-    bool32 _r;
-    int ret = DAQmxReadCounterU32(h,1,timo,
-                                 buff,1,&read,&_r);
-    if (ret!=0) msg = getErrorMessage(ret);
-    return ret;
+    mxReadCounter mxf(h,timo,buff,read,errmsg);
+    daqmx::thread_->call_daqmx(&mxf);
+    return mxf.retval;
 }
+/////////////////////////////////////////////////////////
+struct mxWriteAnalog : public mxFunc
+{
+    const double& timo;
+    const double* buff;
+    int32& written;
+    virtual void operator()()
+    {
+        retval = DAQmxBaseWriteAnalogF64(h,1,FALSE,timo,DAQmx_Val_GroupByChannel,
+                                    buff,&written,NULL);
+        checkError();
+    }
+
+    mxWriteAnalog(TaskHandle h, const double &atimo, const double* abuff, int32& aread, QString& msg) : mxFunc(h,msg),
+        timo(atimo), buff(abuff), read(aread)
+    {}
+};
 int daqmx::writeAnalog(TaskHandle h, double timo, const double *buff, int32 &written, QString &msg)
 {
-    bool32 _r;
-    int ret = DAQmxWriteAnalogF64(h,1,FALSE,timo,DAQmx_Val_GroupByChannel,
-                                 buff,&written,&_r);
-    if (ret!=0) msg = getErrorMessage(ret);
-    return ret;
+    mxWriteAnalog mxf(h,timo,buff,read,errmsg);
+    daqmx::thread_->call_daqmx(&mxf);
+    return mxf.retval;
 }
+/////////////////////////////////////////////////////////
+struct mxWriteDigital : public mxFunc
+{
+    const double& timo;
+    const uInt32* buff;
+    int32& written;
+    virtual void operator()()
+    {
+        retval = DAQmxBaseWriteDigitalU32(h,1,FALSE,timo,DAQmx_Val_GroupByChannel,
+                                    buff,&written,NULL);
+        checkError();
+    }
+
+    mxWriteDigital(TaskHandle h, const double &atimo, const uInt32* abuff, int32& aread, QString& msg) : mxFunc(h,msg),
+        timo(atimo), buff(abuff), read(aread)
+    {}
+};
 int daqmx::writeDigital(TaskHandle h, double timo, const uInt32* buff, int32 &written, QString &msg)
 {
-    bool32 _r;
-    int ret = DAQmxWriteDigitalU32(h,1,FALSE,timo,DAQmx_Val_GroupByChannel,
-                                 buff,&written,&_r);
-    if (ret!=0) msg = getErrorMessage(ret);
-    return ret;
+    mxWriteDigital mxf(h,timo,buff,read,errmsg);
+    daqmx::thread_->call_daqmx(&mxf);
+    return mxf.retval;
 }
 
